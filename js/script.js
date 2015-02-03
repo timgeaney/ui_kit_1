f8a34598a1a99320c8ea605e898667f4842c22fb
tim geaney <timgeaney24@gmail.com>
2015-02-03 08:41:38 +0000

    tabs

--- js/script.js
+++ js/script.js
@@ -27,4 +27,26 @@ $( function() {
 		$this.addClass( "animated " + $this.attr( 'id' ) );
 	} );
 
+	/**
+	 * Tabs.
+	 */
+	$( ".tabs-links a" ).on( "click", function( e ) {
+		e.preventDefault();
+
+		// Get the target tab.
+		var newTab = $( this ).attr( "href" );
+
+		// Show the targeted tab content while hiding the rest.
+		$( newTab ).show().siblings().hide();
+
+		// Add the active class to the parent list item while removing it from the rest.
+		$( this ).parent( "li" ).addClass( "active" ).siblings().removeClass( "active" );
+	} );
+	 
+
+
+
+
+
+
 	} );
